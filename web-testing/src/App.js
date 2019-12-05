ebe99250d83b1e8cc0caea985d51f5e07a2ba826
nathan-saygers <nathan.saygers@gmail.com>
2019-12-05 14:08:21 -0800

    basic pitch count functionality done

--- web-testing/src/App.js
+++ web-testing/src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard";
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+
+  const handlePitch = pitchResult => {
+    switch (pitchResult) {
+      case "strike":
+        if (strikes > 1) {
+          setStrikes(0);
+          setBalls(0);
+          break;
+        } else {
+          setStrikes(strikes + 1);
+          break;
+        }
+
+      case "ball":
+        if (balls > 2) {
+          setStrikes(0);
+          setBalls(0);
+          break;
+        } else {
+          setBalls(balls + 1);
+          break;
+        }
+
+      case "foul":
+        if (strikes > 1) {
+          break;
+        } else {
+          setStrikes(strikes + 1);
+          break;
+        }
+
+      case "hit":
+        setStrikes(0);
+        setBalls(0);
+        break;
+
+      default:
+        console.log('that wasnt right');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display balls={balls} strikes={strikes} />
+      <Dashboard handlePitch={handlePitch}/>
     </div>
   );
 }
